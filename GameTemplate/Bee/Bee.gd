3b83de35788a0e0852c5069eba14f11ead275318
Tomas Sarquis <tsarquis88@gmail.com>
2022-06-24 18:55:22 -0300

    Add: Player, Bee & FirstLevel

---
+++ GameTemplate/Bee/Bee.gd
@@ -0,0 +1,35 @@
+extends KinematicBody2D
+
+export var MAX_HP = 100
+export var NORMAL_SPEED = 3
+
+var speed = NORMAL_SPEED
+var hp = MAX_HP
+
+var directions = ["Right", "RightDown", "Down", "LeftDown", "Left", "LeftUp", "Up", "RightUp"]
+var current_direction: String = "Down" setget set_current_dir, get_current_dir
+var facing = Vector2() setget set_facing, get_facing
+
+func set_current_dir(new_dir: String):
+	current_direction = new_dir
+
+func get_current_dir() -> String:
+	return current_direction
+
+func get_dir(index: int) -> String:
+	return directions[index]
+
+func set_facing(new_facing: Vector2):
+	facing = new_facing
+
+func get_facing() -> Vector2:
+	return facing
+
+func get_animationSprite () -> Node:
+	return $AnimatedSprite
+
+func _ready():
+	pass
+
+func _process(_delta):
+	pass
---
+++ GameTemplate/Player/RunPlayer.gd
@@ -0,0 +1,70 @@
+extends State
+# MOVEMENT FROM:
+# 	https://godotengine.org/qa/31522/how-to-apply-sprite-for-8-direction-movement
+
+onready var player = self.get_node('../../')
+onready var animationSprite = self.get_node('../../AnimatedSprite')
+
+
+
+func _ready():
+	pass
+
+
+# Virtual function. Receives events from the `_unhandled_input()` callback.
+func handle_input(_event: InputEvent) -> void:
+	pass
+
+
+# Virtual function. Corresponds to the `_process()` callback.
+func update(_delta: float) -> void:
+	if !Input.is_action_pressed("move_left") and !Input.is_action_pressed("move_up") and !Input.is_action_pressed("move_right") and !Input.is_action_pressed("move_down"):
+		print_debug("PLAYER: Run to Idle")
+		state_machine.transition_to("Idle")
+
+
+# Virtual function. Corresponds to the `_physics_process()` callback.
+func physics_update(_delta: float) -> void:
+	var move_direction = Vector2()
+
+	var LEFT = Input.is_action_pressed("move_left")
+	var RIGHT = Input.is_action_pressed("move_right")
+	var UP = Input.is_action_pressed("move_up")
+	var DOWN = Input.is_action_pressed("move_down")
+
+	move_direction.x = int(RIGHT) - int(LEFT)
+	move_direction.y = int(DOWN) - int(UP)
+
+	if LEFT || RIGHT || UP || DOWN:
+		player.set_facing(move_direction)
+	
+	player.set_current_dir(direction2str(player.facing))
+	
+	player.move_and_collide(move_direction * player.speed)
+	
+	var animation = player.get_current_dir() + "Run"
+	if animationSprite.get_animation() != animation:
+		animationSprite.play(animation)
+
+
+func direction2str(direction):
+	var angle = direction.angle()
+	if angle < 0:
+		angle += 2 * PI
+	var index = round(angle / PI * 4)
+	return player.get_dir(index)
+
+# Virtual function. Called by the state machine upon changing the active state. The `msg` parameter
+# is a dictionary with arbitrary data the state can use to initialize itself.
+func enter(_msg := {}) -> void:
+	pass
+
+# Virtual function. Called by the state machine before changing the active state. Use this function
+# to clean up the state.
+func exit() -> void:
+	pass
+
+
+
+
+
